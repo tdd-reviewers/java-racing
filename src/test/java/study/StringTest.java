5abc648ba6c739787b233c4d0b6773cad2fcc78c
povia <47127607+povia@users.noreply.github.com>
2023-03-30 12:47:43 +0900

    test: 문자 제거 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -15,9 +15,18 @@ public class StringTest {
 
     @Test
     @DisplayName("'1'을 ','로 split 했을 시 1만을 포함하는 배열 반환")
-    void splitV2Test() {
+    void splitNothingTest() {
         String value = "1";
         var targetValue = new String[]{"1"};
         assertThat(value.split(",")).isEqualTo(targetValue);
+    }
+
+    @Test
+    @DisplayName("문자열에서 문자 제거 테스트")
+    void substringTest() {
+        String value = "(1,2)";
+        var expectedResult = "1,2";
+
+        assertThat(value.substring(1, value.length()-1)).isEqualTo(expectedResult);
     }
 }

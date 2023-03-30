6aadccf6f35dd197d973955cc8cbff6d31fffb45
povia <47127607+povia@users.noreply.github.com>
2023-03-30 13:01:59 +0900

    test: 문자열에서 문자 반환 성공 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -17,8 +17,8 @@ public class StringTest {
     @DisplayName("'1'을 ','로 split 했을 시 1만을 포함하는 배열 반환")
     void splitNothingTest() {
         String value = "1";
-        var targetValue = new String[]{"1"};
-        assertThat(value.split(",")).isEqualTo(targetValue);
+        var expectedResult = new String[]{"1"};
+        assertThat(value.split(",")).containsExactly(expectedResult);
     }
 
     @Test
@@ -28,5 +28,15 @@ public class StringTest {
         var expectedResult = "1,2";
 
         assertThat(value.substring(1, value.length()-1)).isEqualTo(expectedResult);
+    }
+
+    @Test
+    @DisplayName("문자열에서 특정 위치 문자 반환 테스트")
+    void successGetCharacterTest() {
+        String value = "abc";
+
+        assertThat(value.charAt(0)).isEqualTo('a');
+        assertThat(value.charAt(1)).isEqualTo('b');
+        assertThat(value.charAt(2)).isEqualTo('c');
     }
 }

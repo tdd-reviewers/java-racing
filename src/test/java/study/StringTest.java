b4143ed8d104aeb628434594d42da783b67bfd2b
povia <47127607+povia@users.noreply.github.com>
2023-03-30 12:43:47 +0900

    test: 1 split test 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,14 +1,23 @@
 package study;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
 
     @Test
     @DisplayName("분리 테스트")
     void splitTest() {
-        Assertions.assertThat("1,2".split(",")).contains("1");
+        assertThat("1,2".split(",")).contains("1");
+    }
+
+    @Test
+    @DisplayName("'1'을 ','로 split 했을 시 1만을 포함하는 배열 반환")
+    void splitV2Test() {
+        String value = "1";
+        var targetValue = new String[]{"1"};
+        assertThat(value.split(",")).isEqualTo(targetValue);
     }
 }
